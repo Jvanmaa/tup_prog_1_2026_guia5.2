916086572fa1eb2667e164266d6f59cd325528e5
Jvanmaa <juanmv2806@gmail.com>
2026-05-22 23:39:42 -0300

    Ejercicio 2

---
+++ Guia5.2/Ejercicio2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+
+            Console.WriteLine("Ingrese el número a evaluar");
+            num = Convert.ToDouble(Console.ReadLine());
+
+            if (num < 0)
+            {
+                Console.WriteLine("El número es negativo");
+            }
+            else
+            {
+                if (num == 0)
+                {
+                    Console.WriteLine("El número es cero");
+                }
+                else
+                {
+                    Console.WriteLine("El número es positivo");
+                }
+
+            }
+        }
+    }
+}
